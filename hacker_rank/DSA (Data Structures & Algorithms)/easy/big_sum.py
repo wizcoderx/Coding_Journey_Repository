dee4ec02711de8bc949db841f27dae278d7adf7b
wizcoderx <wizcoderx@gmail.com>
2024-11-17 21:52:51 +0530

    dsa updated

---
+++ hacker_rank/DSA (Data Structures & Algorithms)/easy/big_sum.py
@@ -0,0 +1,37 @@
+'''
+In this challenge, you are required to calculate and print the sum of the elements in an array, keeping in mind that some of those integers may be quite large.
+
+https://www.hackerrank.com/challenges/a-very-big-sum
+'''
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'aVeryBigSum' function below.
+#
+# The function is expected to return a LONG_INTEGER.
+# The function accepts LONG_INTEGER_ARRAY ar as parameter.
+#
+
+def aVeryBigSum(ar):
+    result = sum(ar)
+    return result
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    ar_count = int(input().strip())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = aVeryBigSum(ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
