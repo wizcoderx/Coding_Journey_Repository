fcdf4af00a37fb7ef155b8aae5fd254ee0a5efe6
wizcoderx <wizcoderx@gmail.com>
2024-11-24 00:11:50 +0530

    30 days in cpp started

---
+++ hacker_rank/30_days_of_code/cpp/day_0.cpp
@@ -0,0 +1,17 @@
+//https://www.hackerrank.com/challenges/30-hello-world
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    std::string n;
+    std::getline(std::cin, n);
+    cout<<"Hello, World."<<"\n";
+    cout<<n<<endl;
+    return 0;
+}
