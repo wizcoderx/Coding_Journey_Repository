8b68c8ea780cd9cea86c1c8d6c35b8b0a6b8b499
wizcoderx <wizcoderx@gmail.com>
2024-12-15 15:53:14 +0530

    code 1.9.py updated

---
+++ python_book/Chapter_1/Reinforcement/R-1.9.py
@@ -0,0 +1,10 @@
+'''
+What parameters should be sent to the `range` constructor, to produce a range with values `50`, `60`, `70`, `80`?
+'''
+
+listx= []
+for i in range(50,90,10):
+    print(i)
+    listx.append(i)
+
+print(listx)
