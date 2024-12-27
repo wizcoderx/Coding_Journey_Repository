7120635c4d796fc8e0a7f4f5e7388ddf46232609
wizcoderx <wizcoderx@gmail.com>
2024-12-27 21:07:48 +0530

    got 1st batch in sql

---
+++ SQL/Basic/revising_select_query.sql
@@ -0,0 +1,9 @@
+--https://www.hackerrank.com/challenges/revising-the-select-query
+
+/*
+Enter your query here.
+Please append a semicolon ";" at the end of the query and enter your query in a single line to avoid error.
+*/
+
+select * from city
+where population > 100000 and countrycode = 'USA';
