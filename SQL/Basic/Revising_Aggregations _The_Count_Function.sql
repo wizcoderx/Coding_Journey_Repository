13d51a833a2489c9219cc7a79ca75b82c4596cc4
wizcoderx <wizcoderx@gmail.com>
2024-12-27 23:14:40 +0530

    more

---
+++ SQL/Basic/Revising_Aggregations _The_Count_Function.sql
@@ -0,0 +1,9 @@
+--https://www.hackerrank.com/challenges/revising-aggregations-the-count-function
+
+/*
+Enter your query here.
+Please append a semicolon ";" at the end of the query and enter your query in a single line to avoid error.
+*/
+
+select count(*) from city
+where population>100000;
