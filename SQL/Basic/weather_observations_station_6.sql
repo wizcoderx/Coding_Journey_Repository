7f37823b07a7a77db74d934314acd44581deebc9
wizcoderx <wizcoderx@gmail.com>
2024-12-29 21:15:18 +0530

    only one today

---
+++ SQL/Basic/weather_observations_station_6.sql
@@ -0,0 +1,8 @@
+--https://www.hackerrank.com/challenges/weather-observation-station-6
+
+/*
+Enter your query here.
+Please append a semicolon ";" at the end of the query and enter your query in a single line to avoid error.
+*/
+select distinct city from station
+where city like "a%" or city like "e%" or city like "i%" or city like "o%" or city like "u%";
