7ac6e0bc073cbbfb17a378e46865987f56dda10e
wizcoderx <wizcoderx@gmail.com>
2025-01-01 21:02:43 +0530

    small change

--- interview_preparation/capg/Coding/standard_role/Easy/reverse_a_string.cpp
+++ interview_preparation/capg/Coding/standard_role/Easy/reverse_a_string.cpp
@@ -6,7 +6,6 @@ using namespace std;
 
 string reverseString(string s) {
     int n = s.length();
-    string reversed = "";
     for (int i = 0; i < n/2; i++ ) {
         char temp = s[i];
         s[i] = s[n-i-1];
