c2dbf54de6c5a44c57dd3064470666e3f05d9103
wizcoderx <wizcoderx@gmail.com>
2025-01-10 23:29:00 +0530

    did one only

---
+++ interview_preparation/idolize_solutions/coding/set2_advance/python_conditional_statement.py
@@ -0,0 +1,7 @@
+#Write a Python function that returns the maximum number from a given list of integers.
+
+def find_max(lst):
+    return max(lst)
+
+# Test the function
+print(find_max([1, 2, 3, 4, 5]))  # Output: 5
