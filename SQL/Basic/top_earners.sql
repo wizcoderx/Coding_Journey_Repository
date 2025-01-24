0c872e727e0b62764c3321118b2f0b704dfc22de
wizcoderx <wizcoderx@gmail.com>
2025-01-24 16:27:54 +0530

    added some extra comments

--- SQL/Basic/top_earners.sql
+++ SQL/Basic/top_earners.sql
@@ -4,3 +4,8 @@ SELECT MAX(SALARY*MONTHS), COUNT(*)
 FROM EMPLOYEE
 WHERE (SALARY*MONTHS) = (SELECT MAX(SALARY*MONTHS)
                          FROM EMPLOYEE);
+
+                        -- This query selects the employee ID, first name, last name, and department name
+                        -- from the employees and departments tables. It performs an inner join on the
+                        -- department_id field to match employees with their respective departments.
+                        -- The results are ordered by the employee's last name in ascending order.
